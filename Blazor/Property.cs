f3dcc2b8946eb59de397a485a8602ba627f716c0
Onegodx <113604166+Onegodx@users.noreply.github.com>
2024-10-26 05:17:43 +0200

    Добавьте файлы проекта.

---
+++ Blazor/Property.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? SaleDate { get; set; }
+        public string Status { get; set; }
+        public int SellerId { get; set; }
+        public int RealtorId { get; set; }
+        public Seller Seller { get; set; }
+        public Realtor Realtor { get; set; }
+    }
+}
